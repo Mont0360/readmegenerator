eff19c8d95ffe321d4f898b5762d714bb219a180
Mont0360 <mont0360@umn.edu>
2020-05-30 22:06:22 -0500

    update index.js generateMardown.js ?

---
+++ generateMarkdown.js
@@ -0,0 +1,41 @@
+const generateMarkdown = data => {
+  // return JSON.stringify(data)
+  return `
+# ${data.title.toUpperCase()}
+
+<a href="${data.link}" style="float:right"><img src="${data.avatar}" alt="profile-pic" title="${data.name}" width="100" height="100"></a>
+
+![License: ${(data.lic) ? data.lic : 'MIT'}](https://img.shields.io/badge/License-${(data.lic) ? data.lic : 'MIT'}-brightgreen)
+
+
+
+
+
+## Repository by  ${data.name}\n\n\n
+
+
+
+
+---
+
+## ${data.rmTitle}\n\n
+
+## Description:\n${data.rmDesc}\n\n
+
+## URL:\n${data.rmUrl}\n\n
+
+## Installation:\n${data.inst}\n\n
+
+## Usage:\n${data.use}\n\n
+
+## License:\n${data.rmLic}\n\n
+
+## Contributors:\n${data.con}\n\n
+
+## Tests:\n${data.test}\n\n
+
+## Questions:\n${data.qs}\n\n
+`
+}
+
+module.exports = generateMarkdown
